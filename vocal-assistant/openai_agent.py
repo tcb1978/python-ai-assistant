c7051710a4d3be8ea8ccb67f637b1bc82d963434
Matthew Eldredge <matthew.eldredge@photon.care>
2025-04-03 16:23:57 -0700

    feat(python-ai-assistant): adds get resopnse from chat gpt

--- vocal-assistant/openai_agent.py
+++ vocal-assistant/openai_agent.py
@@ -1,6 +1,25 @@
-class OpenAIAgent:
-    def __init__(self):
-        pass
+import openai
+from dotenv import load_dotenv
+import os
 
-    def get_response(self):
-        pass
+# Load environment variables from .env file
+load_dotenv()
+# Set OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+class OpenAIAgent:
+    def __init__(self, model="gpt-3.5-turbo"):
+        self.model = model
+
+    def get_response(self, command):
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": "You are a vocal assistant. You have to answer in simple, efficient and concise manner. Your answer should not take nore than 30 seconds to say out loud."},
+                {"role": "user", "content": command},
+            ],
+        )
+
+        assistant_response = response['choices'][0]['message']['content']
+
+        return assistant_response
